56cbc51b45defafaf44b6a20146910275499f5ee
wys010812 <209306976@qq.com>
2021-10-30 20:26:11 +0800

    Error creating bean with name 'configurationPropertiesBeans' defined 解决异常，无法启动的原因就是springboot版本太高与springcloud推荐版本不一致

--- src/test/java/cn/codepure/springsecurityoauth2demo/Springsecurityoauth2demoApplicationTests.java
+++ src/test/java/cn/codepure/springsecurityoauth2demo/Springsecurityoauth2demoApplicationTests.java
@@ -1,6 +1,6 @@
 package cn.codepure.springsecurityoauth2demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
