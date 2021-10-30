9ae4793db9d2b66dd83349b57d9694d1306291de
wys010812 <209306976@qq.com>
2021-10-30 21:22:42 +0800

    密码模式调试成功

--- src/main/java/cn/codepure/springsecurityoauth2demo/config/AuthorizationServerConfig.java
+++ src/main/java/cn/codepure/springsecurityoauth2demo/config/AuthorizationServerConfig.java
@@ -1,11 +1,14 @@
 package cn.codepure.springsecurityoauth2demo.config;
 
+import cn.codepure.springsecurityoauth2demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /**
  * 授权服务器配置
@@ -16,6 +19,23 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private PasswordEncoder pwd;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 使用密码模式所需要的配置
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userService);
+    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -31,6 +51,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 // 配置申请的权限范围
                 .scopes("all")
                 // 配置grant_type，表示授权类型
-                .authorizedGrantTypes("authorization_code");
+                .authorizedGrantTypes("password");
     }
 }
