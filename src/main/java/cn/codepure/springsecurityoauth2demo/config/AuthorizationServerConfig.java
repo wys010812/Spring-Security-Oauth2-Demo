7ebebbe3e74f375d3167fa3ce8f7c66241b926f7
wys010812 <209306976@qq.com>
2021-10-30 23:52:16 +0800

    配置JWT

--- src/main/java/cn/codepure/springsecurityoauth2demo/config/AuthorizationServerConfig.java
+++ src/main/java/cn/codepure/springsecurityoauth2demo/config/AuthorizationServerConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * 授权服务器配置
@@ -28,6 +30,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private UserService userService;
 
+    @Autowired
+    @Qualifier("jwtTokenStore")
+    private TokenStore tokenStore;
+
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+
     /**
      * 使用密码模式所需要的配置
      * @param endpoints
@@ -36,7 +45,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager)
-                .userDetailsService(userService);
+                .userDetailsService(userService)
+                // 配置存储令牌策略
+                .tokenStore(tokenStore)
+                .accessTokenConverter(jwtAccessTokenConverter);
     }
 
     @Override
---
+++ src/main/java/cn/codepure/springsecurityoauth2demo/config/JwtTokenStoreConfig.java
@@ -0,0 +1,27 @@
+package cn.codepure.springsecurityoauth2demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * JwtToken配置类
+ */
+@Configuration
+public class JwtTokenStoreConfig {
+
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        // 配置Jwt使用的密钥
+        accessTokenConverter.setSigningKey("test_key");
+        return accessTokenConverter;
+    }
+}
