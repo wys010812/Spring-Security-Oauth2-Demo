5385e27fdc473be6ddfaf697b2d4aa9695024ac1
wys010812 <209306976@qq.com>
2021-10-30 14:50:09 +0800

    导入依赖

---
+++ src/main/java/cn/codepure/springsecurityoauth2demo/Springsecurityoauth2demoApplication.java
@@ -0,0 +1,13 @@
+package cn.codepure.springsecurityoauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurityoauth2demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springsecurityoauth2demoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codepure/springsecurityoauth2demo/Springsecurityoauth2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codepure.springsecurityoauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurityoauth2demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
